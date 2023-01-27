b0ade2afa08333c559b10909f756f570d335b6f7
amircodel <amirali.mohammadi890@gmail.com>
2023-01-28 00:00:04 +0330

    2301272359

--- jadi_Project/source.py
+++ jadi_Project/source.py
@@ -1,7 +1,18 @@
 import hashlib
 import csv
-
-def hash_password_hack(input_file_name, output_file_name):
-    
-
-
+from collections import OrderedDict
+def hash_password_hack(input_file_name,output_file_name):
+    with open(input_file_name) as fin :
+        f = csv.reader(fin)
+        f = OrderedDict(f)
+    passcodes = OrderedDict()
+    for i in range(1000,10000):
+        hashi = hashlib.sha256(str(i).encode())
+        hashi = hashi.hexdigest()
+        passcodes[hashi] = str(i)
+    with open(output_file_name, 'w', newline='') as fou :
+        for k,v in f.items():
+            password = passcodes.get(v)
+            w = csv.writer(fou)
+            w.writerow([k,password])
+hash_password_hack('example.csv','xxx.csv')
